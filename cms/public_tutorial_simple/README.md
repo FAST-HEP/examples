c13659044bd9ad994cf5ad4641aa89d2c7c9e2da
kreczko <lkreczko@googlemail.com>
2023-07-05 11:54:02 +0100

    feat: add simplified version of the CMS tutorial for testing only

---
+++ cms/public_tutorial_simple/README.md
@@ -0,0 +1,40 @@
+# CMS Public HEP Tutorial
+
+An example of the public CMS tutorial can be found on [CERN Gitlab](https://gitlab.cern.ch/fast-hep/public/fast_cms_public_tutorial/).
+
+It implements the following workflow:
+
+```mermaid
+graph LR;
+    input_files([download input files])
+    curator([run fast-curator])
+    carpenter([run fast-carpenter])
+    plotter([run fast-plotter])
+    input_files --> curator
+    curator --> carpenter
+    carpenter --> plotter
+```
+
+The corresponding Github actions workflow is available in [.github/workflows/cms-tutorial.yml](https://github.com/FAST-HEP/examples/blob/main/.github/workflows/cms-tutorial.yml)
+
+## To run
+
+```bash
+
+EXAMPLE=cms/public_tutorial
+fasthep download --json ${EXAMPLE}/remote_data.json --destination data/${EXAMPLE}
+
+export PYTHONPATH=$PWD/${EXAMPLE}:$PYTHONPATH
+
+fast_carpenter \
+  --outdir=output/${EXAMPLE} \
+  ${EXAMPLE}/data.yml \
+  ${EXAMPLE}/processing.yml \
+  --mode=parsl:local
+```
+
+or (via `fast-cli`):
+
+```bash
+fasthep carpenter ${EXAMPLE}/data.yml ${EXAMPLE}/processing.yml -o output/${EXAMPLE}
+```
---
+++ cms/public_tutorial_simple/cms_hep_tutorial/__init__.py
@@ -0,0 +1,56 @@
+import awkward as ak
+import vector
+import numpy as np
+
+from fast_carpenter.tree_adapter import ArrayMethods
+
+class DiObjectMass():
+    def __init__(self, name, out_dir, collection="Muon", mask=None, out_var=None):
+        self.name = name
+        self.out_dir = out_dir
+        self.mask = mask
+        self.collection = collection
+
+        self.branches = [self.collection + "_" + var for var in ["Px", "Py", "Pz", "E"]]
+        if out_var:
+            self.out_var = out_var
+        else:
+            self.out_var = "Di{}_Mass".format(collection)
+
+    def event(self, chunk):
+        # Get the data as a pandas dataframe
+        px, py, pz, energy = chunk.tree.arrays(self.branches, outputtype=tuple)
+
+        # Rename the branches so they're easier to work with here
+        if self.mask:
+            mask = chunk[self.mask]
+            px = px[mask]
+            py = py[mask]
+            pz = pz[mask]
+            energy = energy[mask]
+
+        # Find the second object in the event (which are sorted by Pt)
+        has_two_obj = ArrayMethods.counts(px) > 1
+
+        # Calculate the invariant mass
+        p4_0 = vector.array({
+            "x": px[has_two_obj][:, 0],
+            "y": py[has_two_obj][:, 0],
+            "z": pz[has_two_obj][:, 0],
+            "t": energy[has_two_obj][:, 0],
+        })
+        p4_1 = vector.array({
+            "x": px[has_two_obj][:, 1],
+            "y": py[has_two_obj][:, 1],
+            "z": pz[has_two_obj][:, 1],
+            "t": energy[has_two_obj][:, 1],
+        })
+        di_object = p4_0 + p4_1
+
+        # insert nans for events that have fewer than 2 objects
+        masses = np.full(len(chunk.tree), np.nan)
+        masses[has_two_obj] = di_object.tau
+
+        # Add this variable to the tree
+        chunk.tree.new_variable(self.out_var, masses)
+        return True
